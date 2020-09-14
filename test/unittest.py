6c4f7d2e4bc0d34e27fed2c53fff7639671f229c
root <root@jenkins.c.ofir-288912.internal>
2020-09-14 09:39:51 +0000

    ANOTHER SYNTAX ERROR

--- test/unittest.py
+++ test/unittest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import httplib
-def check_status{}:
+def check_status():
     c = httplib.HTTPConnection('http://35.188.146.83:5000')
     c.request("HEAD", '')
     if c.response().status == 200:
@@ -8,4 +8,4 @@ def check_status{}:
     else:
         return 0
 
-check_status{}
+check_status()
