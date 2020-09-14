ee1a239bcad765e7d9f92323311f3315fab9ddd7
root <root@jenkins.c.ofir-288912.internal>
2020-09-14 11:04:09 +0000

    added line to docker file

--- test/unittest.py
+++ test/unittest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import requests
 def check_status():
-    c = requests.get('http://35.188.146.83:5000')
+    c = requests.get('http://35.188.146.83:80')
     if '200' in str(c): 
         return 1
     else:
