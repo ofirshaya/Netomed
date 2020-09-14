847560af760a1bf8459ecfee242261beddd4dbe1
root <root@jenkins.c.ofir-288912.internal>
2020-09-14 09:38:04 +0000

    another syntax error

--- test/unittest.py
+++ test/unittest.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import httplib
-c = httplib.HTTPConnection('http://35.188.146.83:5000')
-c.request("HEAD", '')
-if c.response().status == 200:
-    return 1
-else:
-    return 0
+def check_status{}:
+    c = httplib.HTTPConnection('http://35.188.146.83:5000')
+    c.request("HEAD", '')
+    if c.response().status == 200:
+        return 1
+    else:
+        return 0
 
+check_status{}
